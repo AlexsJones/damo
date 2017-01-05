38bf0f768565c6112f7b3d9907105a7a80da45ca
AlexsJones <tibbar@freedommail.ch>
2017-01-05 15:44:52 +0000

    pushing updates

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -2,7 +2,7 @@
  *     File Name           :     src/main/main.cpp
  *     Created By          :     anon
  *     Creation Date       :     [2016-02-16 14:58]
- *     Last Modified       :     [2017-01-04 09:30]
+ *     Last Modified       :     [2017-01-04 17:53]
  *     Description         :      
  **********************************************************************************/
 
@@ -19,11 +19,8 @@ int main(int argc, char **argv) {
 
   shared_ptr<Engine> eng = make_shared<Engine>(640,480,false);
 
-  string imagePath = getResourcePath("test/damo.png");
 
-  SDL_Texture *tex = eng->loadTextureFromFile(imagePath);
-
-  shared_ptr<Actor> a = make_shared<Actor>(20,20,eng->getRenderer(),"test/damo.png");
+  shared_ptr<Actor> a = make_shared<Actor>(20,20,eng->getRenderer(),"test/image.png");
 
   shared_ptr<IScene> s = make_shared<Scene>();
   
