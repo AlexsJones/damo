3806fc6f13be5db314f6d15b14246da48589e0da
AlexsJones <tibbar@freedommail.ch>
2017-02-20 13:58:54 +0000

    fixed compilation errors

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -2,7 +2,7 @@
  *     File Name           :     src/main/main.cpp
  *     Created By          :     anon
  *     Creation Date       :     [2016-02-16 14:58]
- *     Last Modified       :     [2017-01-12 09:51]
+ *     Last Modified       :     [2017-02-20 13:58]
  *     Description         :      
  **********************************************************************************/
 
@@ -19,13 +19,12 @@ int main(int argc, char **argv) {
 
   shared_ptr<Engine> eng = make_shared<Engine>(640,480,false);
 
-
-  shared_ptr<Actor> a = make_shared<Actor>(20,20,eng->getRenderer(),"test/image.png");
+  shared_ptr<Actor> a = make_shared<Actor>(20,20,eng->getRenderer(),"resources/test/image.png");
 
   shared_ptr<IScene> s = make_shared<Scene>();
-  
+
   s->addActor(a);
-  
+
   eng->addScene(s);
 
   while(!eng->IsExiting()) {
